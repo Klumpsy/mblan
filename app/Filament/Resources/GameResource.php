99c314f767a33789b4958e6edc478c7cd9008e9a
bart <bart_klumperman@Live.nl>
2025-05-13 23:37:50 +0200

    added filament

---
+++ app/Filament/Resources/GameResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GameResource\Pages;
+use App\Filament\Resources\GameResource\RelationManagers;
+use App\Models\Game;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GameResource extends Resource
+{
+    protected static ?string $model = Game::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                    
+                TextInput::make('year_of_release')
+                    ->required(),
+                FileUpload::make('image')
+                    ->disk('public')
+                    ->directory('games')
+                    ->visibility('public'),
+                RichEditor::make('content')
+                    ->toolbarButtons([
+                        'attachFiles',
+                        'blockquote',
+                        'bold',
+                        'bulletList',
+                        'codeBlock',
+                        'h2',
+                        'h3',
+                        'italic',
+                        'link',
+                        'orderedList',
+                        'redo',
+                        'strike',
+                        'underline',
+                        'undo',
+                    ])
+                   
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')->disk('public')->visibility('public')->size(40),
+                TextColumn::make('name'),
+                TextColumn::make('year_of_release'),
+                TextColumn::make('likes'),  
+                TextColumn::make('linkToWebsite'),
+                TextColumn::make('linkToYoutube'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGames::route('/'),
+            'create' => Pages\CreateGame::route('/create'),
+            'edit' => Pages\EditGame::route('/{record}/edit'),
+        ];
+    }
+}
--- resources/views/components/games.blade.php
+++ resources/views/components/games.blade.php
@@ -2,9 +2,6 @@
     class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
     <div class="flex justify-between">
         <x-application-logo class="block h-12 w-auto" />
-        <x-protected-button role="admin" route="games.create" class="btn btn-success">
-            Add new game
-        </x-protected-button>
     </div>
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
