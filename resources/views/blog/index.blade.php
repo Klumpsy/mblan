cb47f3a2fdbae567b603a98616fa9b49074d86f9
Klumpsy <bart_klumperman@live.nl>
2025-06-24 11:54:17 +0200

    removed headers on dashboard and blog page

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h1 class="font-semibold text-xl text-gray-800 dark:text-primary-400 leading-tight">
-            Blog Posts
-        </h1>
-    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-primary-400 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
 
     @if ($user->hasSignedUpForLatestEdition() && $latestBlog)
         <div class="pt-12">
